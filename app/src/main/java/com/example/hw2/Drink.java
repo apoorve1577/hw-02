e5dfdd12d2e17a0ed4002ad7341974034ad5abb3
Trish <trishbandhekar97@outlook.com>
2022-09-10 19:32:11 -0400

    Drink

---
+++ app/src/main/java/com/example/hw2/Drink.java
@@ -0,0 +1,16 @@
+package com.example.hw2;
+
+import java.util.Date;
+
+public class Drink {
+
+    int size;
+    int percent;
+    Date createDatetime;
+
+    public Drink(int size, int percent, Date createDatetime) {
+        this.size = size;
+        this.percent = percent;
+        this.createDatetime = createDatetime;
+    }
+}
